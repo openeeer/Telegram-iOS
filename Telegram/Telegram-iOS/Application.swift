508879f9af0f9f1d7226faeaa065bca780dc2c7f
openeeer <dfxminecraft@gmail.com>
2026-06-25 17:46:07 +0300

    2c-1: embed Phantom.xcframework (gomobile) + link into app + smoke call

--- Telegram/Telegram-iOS/Application.swift
+++ Telegram/Telegram-iOS/Application.swift
@@ -1,6 +1,14 @@
 import UIKit
+import Phantom
 
 @objc(Application) class Application: UIApplication {
+    override init() {
+        super.init()
+        // Stage 2c-1 smoke test: verify the Phantom gomobile framework links and
+        // its exported symbols are callable. Replaced by real wiring in 2c-2.
+        NSLog("[Phantom] binding version: \(PhantommobileBindingVersion())")
+    }
+
     override func sendEvent(_ event: UIEvent) {
         super.sendEvent(event)
     }
